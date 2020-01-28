1d70a1158f1eb6b9992e6b15f64214fa791a775c
Trapman <david.a.shuttleworth@gmail.com>
2020-01-28 13:28:46 -0500

    Create Comments and Headers.r

---
+++ Comments and Headers.r
@@ -0,0 +1,7 @@
+# indicates a comment (i.e. code that doesn't get run)
+
+# CONTROL + ENTER is a short cut that allows you to turn # on and off (i.e. instead of manually deleting them)
+
+# THIS IS A LEVEL 1 HEADER ###################
+## This is a level 2 Header ==================
+### This is a level 3 header -----------------
