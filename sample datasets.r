5b1cbd4c79c5dc2aedd0da7db267eca5a588351a
Trapman <david.a.shuttleworth@gmail.com>
2020-01-28 15:41:20 -0500

    Create sample datasets.r

---
+++ sample datasets.r
@@ -0,0 +1,16 @@
+# R comes with pre-built example datasets that you can play around with
+
+library(datasets)          # to load these example datasets 
+
+?datasets                  # to get more info about it
+library(help = "datasets") # to explore it deeper 
+
+iris                       # this is just the name of one of the datasets contained within "dataset"
+
+UCBAdmissions              # Cal Berkeley Admissions datasets 
+
+Titanic                    # Good for machine learning
+
+state.x77                  # Population Data
+
+swiss                      # Good for predictive models
