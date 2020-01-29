46cf07676fb930b2ca4e201fcaca1ca2c5252678
Trapman <david.a.shuttleworth@gmail.com>
2020-01-29 13:52:28 -0500

    Update importing data from a spreadsheet.r

--- importing data from a spreadsheet.r
+++ importing data from a spreadsheet.r
@@ -1,6 +1,6 @@
-# built-it ways
+# built-it ways of importing (kind of clunky)
 
-# package ways (easier and cleaner)
+# package ways (easier and cleaner, like pacman package)
 
 # INSTALL AND LOAD PACKAGES ################################
 
@@ -15,3 +15,28 @@ pacman:: p_load(pacman, party, trio, tidyverse)
 # tidyverse: for so many reasons (lol)
 
 # LOAD AND PREPARE DATA ########################################
+
+# save data to "df" ("for data frame")
+# rename outcome as "y" (if it helps)
+# specify outcome with df$y
+
+# Import CSV files with readr::read_csv() from tidyverse
+(df <- read_csv("data/StateData.csv"))
+
+# Import other formats with rio::import() from rio
+(df <- import("data/StateData.xlsx") %>% as_tibble()  #tibble() is a kind of df from tidyverse
+ 
+#or...
+ 
+df <- import("data/StateData.xlsx") %>%
+  as_tibble() %>%
+  select(state_code,
+         psychRegions,
+         instagram:modernDance) %>%
+  mutate(psyschRegions = as.factor(psychRegions)) %>%  #as.factor reconstitutes it and let's us do other things with it
+  rename(y = psychRegions) %>%   #just an easy rename
+  print()
+ 
+ # ANALYZE DATA ########################################################
+
+ 
