2e84e6b2bcddc9cd5b67fba9af068e4551aeda2e
Trapman <david.a.shuttleworth@gmail.com>
2020-01-30 15:54:47 -0500

    Create Creating Cluster Charts.r

---
+++ Creating Cluster Charts.r
@@ -0,0 +1,36 @@
+# Help dispay the CONNECTIONS between data in your dataset
+
+# INSTALL AND LOAD PACKAGES #############################
+
+pacman::p_load(pacman, rio, tidyverse)
+# pacman: for loading/unloading packages
+# rio: for importing data
+# tidyverse: for so many reasons
+
+# LOAD AND PREPARE DATA #############################
+
+# Save data to "df"
+# Rename outcome as "y"
+# Specify outcome with df$y
+
+df <- import("data\StateData.xlsx") %>%
+  as.tibble() %>%
+  select(state_code,
+    psychRegions,
+    instagram::modernDance) %>%
+  mutate(psychRegions = as.factor(psychRegions)) %>%
+  rename(y = pyschRegions) %>%
+  print()
+  
+# ANALYZE DATA #######################################
+
+# Calculate Clusters
+hc <- df %>% # Get data
+  dist %>% # Computer distance/dissimilarity matrix
+  hclust # Computer hierarchical cluster
+  
+# Plot dendragram   (this is just the plot of the clusters)
+hc %>% plot(labels ~ dfstate_code)
+
+# Draw boxes around clusters
+hc %>% rect.hcluster(k = 2, border = "gray80")
